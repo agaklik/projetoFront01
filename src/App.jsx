56ee269981ce9b0ec715cd3bda845cc5f4001475
agaklik <112034103+agaklik@users.noreply.github.com>
2023-03-31 22:33:38 -0300

    Segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,12 @@ export function App() {
       <img
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=80"
       alt="cover"
-      />       
-      <h1>cover</h1>
-      <h1>avatar</h1>
+      />
+      <img 
+      className={style.avatar}
+      src="https://github.com/agaklik.png"
+      alt="avatar"
+      />     
       <h1>Descrição/nome</h1>
       <h1>Stack</h1>
       </div>
